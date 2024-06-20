fb010389eb6c6f37a863b2943ce05cdd7abf2731
panduramadhan2 <panduramadhan2@gmail.com>
2024-06-20 16:46:27 +0700

    checkpoint 1 JPA

--- EmployeeApplication/src/main/java/com/example/EmployeeApplication/repository/EmployeeRepository.java
+++ EmployeeApplication/src/main/java/com/example/EmployeeApplication/repository/EmployeeRepository.java
@@ -1,8 +1,9 @@
 package com.example.EmployeeApplication.repository;
 
+import com.example.EmployeeApplication.entity.Employee;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
